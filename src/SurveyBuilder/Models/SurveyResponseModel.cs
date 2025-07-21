054f5b526381daa1254f38db7dc9f5a8996a4e79
Seth Kay Nakashima <SethKay.Nakashima@sony.com>
2025-07-21 14:54:05 +0700

    created survey response model, implementing ways to populate in demoapp

---
+++ src/SurveyBuilder/Models/SurveyResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyBuilder.Models
+{
+    public class SurveyResponseModel
+    {
+        public string SurveyId { get; set; } = string.Empty;
+
+        // Core of the answer map
+        public Dictionary<string, object> Answers { get; set; } = new();
+    }
+}
